c8636537af7a482dc97673b9c53409e67f9f1d24
Sami <Sami_1>
2024-08-03 01:04:19 +0500

    final

---
+++ main.js
@@ -0,0 +1,55 @@
+// TASK-1
+
+var just =prompt ('Please write down your name ')
+
+console.log('My name is ' + just);
+
+
+var just =prompt ('How old are you? ')
+
+console.log('I am  ' + just + ' years old');
+
+
+var just=prompt('21+8= ')
+
+console.log('Task №1: ' + '21+8= '+ 'Your answer is ' + just );
+
+
+var just=prompt('38-5= ')
+
+console.log('Task №2: ' + '38-5= '+ 'Your answer is ' + just );
+
+
+
+var just=prompt('100*4= ')
+
+console.log('Task №3: ' + '100*4= '+ 'Your answer is ' + just );
+
+
+
+var just=prompt('48/6= ')
+
+console.log('Task №4: ' + '48/6= '+ 'Your answer is ' + just );
+
+
+// TASK-2
+
+var just=prompt('17%5= ')
+
+console.log('Task №5: ' + '17%5= '+ 'Your answer is ' + just );
+
+
+var x=+prompt('Write down your first number ')
+
+var y=+prompt('Write down your second number ')
+
+var z=+prompt('Write down your third number ')
+
+console.log('Your arithmetic mean is: ' + (x+y+z)/3);
+
+
+
+
+
+
+
